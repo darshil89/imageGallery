e128ba00c2880348997e18fac2c683e8a05756de
darshil89 <darshilmahraur67@gmail.com>
2023-10-02 10:29:22 +0530

    changed the changes

--- next.config.js
+++ next.config.js
@@ -3,12 +3,7 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com","plus.unplash.com"],
   },
-  target: {
-    // ...other target options
-    metadata: {
-      metadataBase: 'https://image-gallery-roan-alpha.vercel.app/', // Replace with your actual base URL
-    },
-  },
+  
 };
 
 module.exports = nextConfig;
